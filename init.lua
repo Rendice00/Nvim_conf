a4dd35e319e6ea891b959af7f84c1540d5ed6851
Rendice00 <richhornet2000@gmail.com>
2025-07-27 22:31:06 +0200

    First commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+require('options')
+require('plugins')
+require('colorscheme')
+require('keymaps')
+require('lsp')
+vim.cmd [[
+    highlight CursorLine guibg=#2e262d
+]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#69616a', bold=true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#69616a', bold=true })
